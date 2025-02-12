53c7c3ad56c4c276af62ef80aa8fb26647889dd6
jorgeRiosGarcias <jorge.rios@medismart.live>
2025-02-12 20:35:25 -0300

    refactor: :art: format code and comment out unused imports in page.tsx and middleware.ts

--- app/home/enroll/empresa/page.tsx
+++ app/home/enroll/empresa/page.tsx
@@ -5,12 +5,18 @@
 // import { Label } from "@/components/ui/label";
 import { Building2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { CompanyTable } from "@/modules/companies/company-table";
-import EmpresaForm from '@/components/ms/enroll/form-empresa';
+import EmpresaForm from "@/components/ms/enroll/form-empresa";
 
 const EmpresaPage = () => {
-
   return (
     <main className="min-h-screen bg-white px-4 sm:px-6 lg:px-8 w-full">
       <div className="bg-[url('/img/shared/background-banner.png')] bg-cover bg-center bg-no-repeat relative h-24 rounded-xl mb-4 flex flex-col gap-1 items-start justify-center px-8">
@@ -26,21 +32,23 @@ const EmpresaPage = () => {
             <DialogContent>
               <DialogHeader>
                 <DialogTitle>Agregar Pais</DialogTitle>
-                <DialogDescription>Complete los siguientes campos para agregar una empresa.</DialogDescription>
+                <DialogDescription>
+                  Complete los siguientes campos para agregar una empresa.
+                </DialogDescription>
               </DialogHeader>
               <form>
-                //agregar formularios
+                {/* agregar formularios */}
                 <EmpresaForm></EmpresaForm>
-                <Button type="submit" variant="secondary">Guardar</Button>
+                <Button type="submit" variant="secondary">
+                  Guardar
+                </Button>
               </form>
             </DialogContent>
           </Dialog>
-
         </div>
-
       </div>
       <CompanyTable></CompanyTable>
-      </main>
+    </main>
   );
 };
 
