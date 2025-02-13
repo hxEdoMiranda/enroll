ddb114badb42621a2c9ee7389d3f48c398a5853d
jorgeRiosGarcias <jorge.rios@medismart.live>
2025-02-12 21:16:21 -0300

    refactor: :art: update cache control strategy and remove headers configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,19 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, max-age=0",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
