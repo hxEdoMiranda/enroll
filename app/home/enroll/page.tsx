a203da6e04a4781fa8ab26040861b862636cb2ae
Edomiranda <eduardo.miranda@medismart.live>
2025-02-17 14:33:06 -0300

    feat: enroll menu

--- app/home/enroll/page.tsx
+++ app/home/enroll/page.tsx
@@ -1,43 +1,15 @@
 'use client';
 
-import Link from 'next/link';
 import React from 'react';
-
-const MyComponent = () => {
+import EnrollButton from '@/components/ms/enroll/boton-menu';
+const Enroll = () => {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <Link href="enroll/empresa/">
-        <div className="w-full h-24 bg-primary text-primary-foreground flex items-center justify-center">
-          EMPRESA
-        </div>
-      </Link>
-      <Link href="/ruta2">
-        <div className="w-full h-24 bg-primary text-primary-foreground flex items-center justify-center">
-          Botón 2
-        </div>
-      </Link>
-      <Link href="/ruta3">
-        <div className="w-full h-24 bg-primary text-primary-foreground flex items-center justify-center">
-          Botón 3
-        </div>
-      </Link>
-      <Link href="/ruta4">
-        <div className="w-full h-24 bg-primary text-primary-foreground flex items-center justify-center">
-          Botón 4
-        </div>
-      </Link>
-      <Link href="/ruta5">
-        <div className="w-full h-24 bg-primary text-primary-foreground flex items-center justify-center">
-          Botón 5
-        </div>
-      </Link>
-      <Link href="/ruta6">
-        <div className="w-full h-24 bg-primary text-primary-foreground flex items-center justify-center">
-          Botón 6
-        </div>
-      </Link>
+    <div className="p-4 md:p-8 bg-white bg-opacity-20 mr-4 mt-14 rounded-lg">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+        <EnrollButton />
+      </div>
     </div>
   );
 };
 
-export default MyComponent;
+export default Enroll;
---
+++ components/ms/enroll/boton-menu.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { getCompanyConfig } from '@/actions/config';
+
+interface ICompanyConfig {
+    idEmpresa: number;
+    img: string;
+    texto: string;
+    subTexto: string;
+    accion: string;
+    target: string;
+}
+
+const EnrollButton = () => {
+    const [companyConfigs, setCompanyConfigs] = useState<ICompanyConfig[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getCompanyConfig(99);
+                setCompanyConfigs(data);
+            } catch (error) {
+                console.error('Error fetching company config:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            {companyConfigs.map((config) => (
+                <Link
+                    key={config.idEmpresa}
+                    href={config.accion}
+                    target={config.target || '_blank'}
+                    className="w-62 h-36 bg-secondary text-primary-menu rounded-lg flex flex-col items-center justify-center relative no-underline"
+                >
+                    <Image
+                        src={config.img}
+                        alt={config.texto}
+                        width={90}
+                        height={90}
+                        className="absolute top-2 right-2 mt-2 mr-2"
+                    />
+                    <span className="text-lg font-semibold">{config.texto}</span>
+                    <span className="text-sm font-normal text-primary opacity-70">{config.subTexto}</span>
+                </Link>
+            ))}
+        </>
+    );
+};
+
+export default EnrollButton;
