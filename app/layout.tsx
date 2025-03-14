aea91afd4190fa879253378ba7a97ae5dcc06355
jorgeRiosGarcias <154276921+jorgeRiosGarcias@users.noreply.github.com>
2025-03-14 13:41:53 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
-
 export const metadata: Metadata = {
   title: "Back Office - MEDISMART",
   description: "ENROLL 3.0",
