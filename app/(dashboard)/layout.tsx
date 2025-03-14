466a32952f98d9cc6510fc80ed0ef695e6b3a61f
jorgeRiosGarcias <jorge.rios@medismart.live>
2025-03-14 10:33:54 -0300

    feat: agregar ClerkProvider a los layouts de dashboard y enroll para gestión de autenticación

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/modules/shared/header";
 import { Sidebar } from "@/modules/shared/sidebar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default async function DashboardLayout({
   children,
@@ -7,12 +8,14 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Header returnButton={true} />
-        <main className="flex-1 overflow-auto">{children}</main>
+    <ClerkProvider>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Header returnButton={true} />
+          <main className="flex-1 overflow-auto">{children}</main>
+        </div>
       </div>
-    </div>
+    </ClerkProvider>
   );
 }
